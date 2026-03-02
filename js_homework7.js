ee9e11ae029b54356119a8dc57467c62a24417d3
Marina Skvortsova <marinaskvortsova380@gmail.com>
2026-03-02 00:13:09 -0600

    js_homework paranoya

---
+++ js_homework7.js
@@ -0,0 +1,29 @@
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" // Нам такі не підходять
+    },
+];
+const regex = /^[a-z0-9]+(\.[a-z0-9]+)?@(gmail\.com|yahoo\.com)$/i;
+const trustedEmails = [];
+
+for (let i = 0; i < arr.length; i++) {
+const email = arr[i].email;
+
+if (regex.test(email)) {
+    trustedEmails.push(email);
+}
+
+}
+console.log(trustedEmails);
